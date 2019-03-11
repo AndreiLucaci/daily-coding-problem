5463d9fcca777ddc26e567039d2c7fe95f34eb90
Andrei Lucaci <contact@andreilucaci.ro>
2019-03-12 00:56:36 +0200

    fix for 12.03.2019

--- DailyCodingProblem/DailyCodingProblem/2019/March/March12.cs
+++ DailyCodingProblem/DailyCodingProblem/2019/March/March12.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using NUnit.Framework;
 
 namespace DailyCodingProblem._2019.March
 {
@@ -13,91 +14,42 @@ namespace DailyCodingProblem._2019.March
     /// </summary>
     public class March12 : BaseSolution
     {
-        private readonly Dictionary<string, string> _alphabet =
-            Enumerable.Range(97, 26).ToDictionary(x => (x - 96).ToString(), x => ((char)x).ToString());
-
         protected override void Solution()
         {
-            var input = "1234";
+            var input1 = "12";
+            var expectedOutput1 = 2;
+            var result1 = Count(input1);
 
-            nul(input);
 
-            //var result = Decode(input).ToList();
+            var input2 = "111";
+            var expectedOutput2 = 3;
+            var result2 = Count(input2);
+
+            Assert.AreEqual(expectedOutput1, result1);
+            Assert.AreEqual(expectedOutput2, result2);
         }
 
-        private IEnumerable<string> Decode(string input)
+        private int Count(string encodedMessage)
         {
-            var parts = _alphabet.Where(x => input.Contains(x.Key)).OrderBy(x => x.Key.Length)
-                .ToDictionary(x => x.Key, x => x.Value);
+            var count = Enumerable.Repeat(1, 2).ToList();
 
-            var level1 = input.Select(x => x.ToString());
-
-            var level21 = new List<List<string>>();
-            var len = 2;
-            for (var i = 0; i < input.Length - len; i++)
+            for (var i = 2; i <= encodedMessage.Length; i++)
             {
-                var split = Split(input, 2, i);
-                level21.Add(split.ToList());
+                count.Add(0);
+
+                if (encodedMessage[i - 2] > '0')
+                {
+                    count[i] = count[i - 1];
+                }
+
+                if (encodedMessage[i - 2] == '1' ||
+                    encodedMessage[i - 2] == '2' && encodedMessage[i - 1] < '7')
+                {
+                    count[i] += count[i - 2];
+                }
             }
 
-            return null;
-        }
-
-        private void nul(string str)
-        {
-            var accumulator = new List<List<string>>();
-            var idx = 0;
-            var len = 2;
-            while (idx + len <= str.Length)
-            {
-                var acc = new List<string>();
-
-                for (var i = 0; i < idx; i++) acc.Add(str[i].ToString());
-
-                acc.Add(str.Substring(idx, len));
-
-                for (var i = idx + len; i < str.Length; i++) acc.Add(str[i].ToString());
-
-                idx++;
-                accumulator.Add(acc);
-            }
-
-            idx = 0;
-
-            while (idx + len <= str.Length)
-            {
-                var acc = new List<string>();
-
-                for (var i = 0; i < idx; i++) acc.Add(str[i].ToString());
-
-                for (var i = idx; i < (str.Length % 2 == 0 ? str.Length- len : str.Length-1); i += len)
-                    acc.Add(str.Substring(i, len));
-
-                for (var i = idx + len; i < str.Length; i++) acc.Add(str[i].ToString());
-
-                idx++;
-                accumulator.Add(acc);
-            }
-
-
-        }
-
-        static IEnumerable<string> Split(string str, int chunkSize, int start = 0)
-        {
-            var rng = Enumerable.Range(start, str.Length / chunkSize + 1);
-
-            return rng.Select(i => Substr(str, i * chunkSize, chunkSize));
-        }
-
-        static string Substr(string str, int start, int length)
-        {
-            var sb = new StringBuilder();
-            for (var i = start; i < ((start + length) <= str.Length ? start + length : str.Length); i++)
-            {
-                sb.Append(str[i]);
-            }
-
-            return sb.ToString();
+            return count.Last();
         }
     }
 }
